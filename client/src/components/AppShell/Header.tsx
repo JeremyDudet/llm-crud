dd03eaf7bcf2e25f02aaf9d3bac10fbd7056afd4
Jeremy Dudet <j.dudet@gmail.com>
2024-10-06 19:41:23 -0700

    change color of hambuger menu

--- client/src/components/AppShell/Header.tsx
+++ client/src/components/AppShell/Header.tsx
@@ -137,7 +137,7 @@ export default function Header() {
           <div className="-mr-2 flex items-center sm:hidden">
             {/* Mobile menu button */}
             {currentUserEmail ? (
-              <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+              <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-white p-2 hover:bg-gray-100  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                 <span className="absolute -inset-0.5" />
                 <span className="sr-only">Open main menu</span>
                 <Bars3Icon
